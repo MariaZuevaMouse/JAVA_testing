ca0bb996fd70b3b4f3d6fac8e9368df3ee62a816
MariaZuevaMouse <58681283+MariaZuevaMouse@users.noreply.github.com>
2020-05-31 12:27:37 +0300

    home_#5

--- src/main/java/ru/geek/mz/site/at/pages/Page.java
+++ src/main/java/ru/geek/mz/site/at/pages/Page.java
@@ -1,22 +1,32 @@
 package ru.geek.mz.site.at.pages;
 
+import com.google.gson.internal.$Gson$Preconditions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NotFoundException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import static java.lang.Integer.parseInt;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Page extends BasePage {
     @FindBy(className = "gb-header__title")
     private WebElement pageHeader;
-//    public static By headerSelector =  By.className("gb-header__title");
+
 
     private NavigationTab navigationTab;
+
+    private SearchOnPage searchOnPage;
+
+    public SearchOnPage getSearchOnPage() {
+        return searchOnPage;
+    }
 
     public Page(WebDriver driver) {
         super(driver);
         navigationTab = new NavigationTab(driver);
+        searchOnPage = new SearchOnPage(driver);
     }
 
 
@@ -26,7 +36,68 @@ public class Page extends BasePage {
         return this;
     }
 
+    public int checkCount(WebElement checkedElement){
+        return parseInt(checkedElement.getText().split("・")[1]);
+    }
+
+
+
+
     public NavigationTab getNavigationTab(){
         return navigationTab;
+    }
+
+    @FindBy(css = "ul.search-page-tabs > li > a[data-tab='professions']")
+    private WebElement professionTab;
+
+    @FindBy(css = "ul.search-page-tabs > li > a[data-tab='courses']")
+    private WebElement coursesTab;
+
+    @FindBy(css="ul.search-page-tabs > li > a[data-tab='webinars']" )
+    private WebElement webinarsTab;
+
+    @FindBy(css = "ul.search-page-tabs > li > a[data-tab='blogs']")
+    protected WebElement blogTab;
+
+    @FindBy(css = "ul.search-page-tabs > li > a[data-tab='forums']")
+    private WebElement forumTab;
+
+    @FindBy(css ="ul.search-page-tabs > li > a[data-tab='tests']" )
+    private WebElement testsTab;
+
+    @FindBy(css = "ul.search-page-tabs > li > a[data-tab='companies']")
+    private WebElement companiesTab;
+
+    public Page checkTabs(String tabTitle) {
+        switch (tabTitle) {
+            case "professionTab": {
+                professionTab.isDisplayed();
+                break;
+            }
+            case "coursesTab": {
+                coursesTab.isDisplayed();
+                break;
+            }
+            case "webinarsTab": {
+                webinarsTab.isDisplayed();
+                break;
+            }
+            case "blog": {
+                blogTab.isDisplayed();
+                break;
+            }
+            case "forum": {
+                forumTab.isDisplayed();
+                break;
+            }
+            case "test": {
+                testsTab.isDisplayed();
+                break;
+            }
+            default: {
+                throw new NotFoundException("Element not found " + tabTitle);
+            }
+        }
+        return new Page(driver);
     }
 }
